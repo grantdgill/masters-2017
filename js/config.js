b87db1d261ea2bd793997709ac57f12bb126be29
Grant Gill <grant.d.gill@espn.com>
2017-04-06 08:35:49 -0400

    initial commit

---
+++ js/config.js
@@ -0,0 +1,67 @@
+var fantasy = fantasy || {};
+fantasy.config = {
+	tournamentName:"Masters 2017",
+	tournamentId: 2700,
+	teams:[
+		{name: "JackG", golfers:[3448, 308, 3702, 1680, 5579]},
+		{name: "Buzz", golfers:[3448, 5467, 3470, 1680, 569]},
+		{name: "Garrett", golfers:[3448, 5467, 3470, 780, 1222]},
+		{name: "Jetta", golfers: [5467, 9780, 5860, 780, 3351]},
+		{name: "Michael", golfers:[3448, 5467, 3470, 9780, 5860]},
+		// {name: "DaveG", golfers:[1680, 388, 3702, 1293, 4304]},
+		{name: "JR", golfers:[3470, 1680, 5860, 158, 576]},
+		{name: "Richard1", golfers:[3448, 9780, 4848, 5579, 3351]},
+		{name: "Richard2", golfers:[3448, 5467, 308, 9780, 569]},
+		{name: "Beth", golfers:[3448, 3470, 9780, 5860, 1097]},
+		// {name: "Jim1", golfers:[1680, 388, 1293, 569, 5860]},
+		// {name: "Jim2", golfers:[1680, 5467, 308, 3702, 686]},
+		{name: "GrantG", golfers:[3448, 5467, 3470, 3702, 4848]},
+		{name: "Rich1", golfers:[3448, 3470, 308, 9870, 4848]},
+		// {name: "Rich2", golfers:[5467, 388, 5579, 257, 4848]},
+		{name: "Steve", golfers:[3448, 308, 1680, 569, 1293]},
+		{name: "JohnG", golfers:[3448, 5467, 3702, 1222, 5579]},
+		// {name: "MattW", golfers:[5467, 3470, 388, 1293, 1097]},
+		// {name: "MarkT", golfers:[1680, 3702, 3448, 257, 774]},
+		// {name: "LauraB", golfers:[1293, 576, 3470, 1680, 388]}
+	],
+	firstPlaceValue: 50,
+	showCut: false,
+	apiKey: '_____'
+
+	// mcilroy - 3470
+	// spieth - 5467
+	// walker - 446
+	// fowler - 3702
+	// holmes - 1067
+	// mickelson - 308
+	// watson - 780
+	// johnson - 3448
+	// day - 1680
+	// tiger - 462
+	// scott - 388
+	// stenson - 576
+	// reed - 5579
+	// kuchar - 257
+	// snedeker - 1222
+	// rose - 569
+	// dufner - 110
+	// garcia - 158
+	// westwood - 455
+	// oosthuizen - 1293
+	// horschel - 1651
+	// matsuyama - 5860
+	// z. johnson - 686
+	// furyk - 153
+	// kirk - 3449
+	// conners - 9126
+	// shwartzel - 1097
+	// willet - 4304
+	// koepka - 6798
+	// kisner - 2552
+	// justin thomas - 4848
+	// bill haas - 774
+	// rahm - 9780
+	// leishman - 3351
+
+
+};
